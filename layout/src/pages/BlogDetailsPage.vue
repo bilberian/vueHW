5279614eb3e2e98466840c1c36c3892414285775
bilberian <116255118+bilberian@users.noreply.github.com>
2024-03-22 13:22:33 +0300

    Update BlogDetailsPage.vue

--- layout/src/pages/BlogDetailsPage.vue
+++ layout/src/pages/BlogDetailsPage.vue
@@ -3,9 +3,6 @@
     <HeaderComponent></HeaderComponent>
     <BDBannerComponent></BDBannerComponent>
     <BDMainComponent></BDMainComponent>
-    <!-- <BlogBannerComp></BlogBannerComp>
-    <LatestPostComp></LatestPostComp>
-    <ArticlesComponent></ArticlesComponent> -->
     <FooterComponent></FooterComponent> 
   </div>
 </template>
@@ -14,9 +11,6 @@
 import HeaderComponent from './../components/HeaderComponent.vue'
 import BDBannerComponent from './../components/BlogDetailsPageComp/BDBannerComponent.vue'
 import BDMainComponent from './../components/BlogDetailsPageComp/BDMainComponent.vue'
-// import BlogBannerComp from './../components/BlogPageComp/BlogBannerComp.vue'
-// import LatestPostComp from './../components/BlogPageComp/LatestPostComp.vue'
-// import ArticlesComponent from './../components/BlogPageComp/ArticlesComponent.vue'
 import FooterComponent from './../components/FooterComponent.vue'
 
 export default {
@@ -25,9 +19,6 @@ export default {
     HeaderComponent,
     BDBannerComponent,
     BDMainComponent,
-    // BlogBannerComp,
-    // LatestPostComp,
-    // ArticlesComponent,
     FooterComponent
   }
 }
